4001d5e7f3a7022f388437533cd87d5016a904e9
Hoon <yanghoon5@gmail.com>
2023-06-13 22:18:23 +0900

    Inital Commit

---
+++ src/main/java/slim/bootjava/BootJavaApplication.java
@@ -0,0 +1,13 @@
+package slim.bootjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/slim/bootjava/BootJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package slim.bootjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
