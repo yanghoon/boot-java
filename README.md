0e0332eef91d3d7339405ad0849fe2e69369c3d3
Hoon <yanghoon5@gmail.com>
2023-06-13 22:40:33 +0900

    Add HelloController

---
+++ README.md
@@ -0,0 +1,22 @@
+# Spring Boot with Java
+
+A template project for Spring Boot with Java
+
+## Build
+
+```bash
+./gradlew build
+./gradlew build -x test
+```
+
+## Test
+
+```bash
+./gradlew test --tests BootJavaApplicationTest
+./gradlew test --tests BootJavaApplicationTest --rerun
+```
+## Run
+
+```bash
+./gradlew bootRun
+```
---
+++ src/main/java/slim/bootjava/rest/HelloController.java
@@ -0,0 +1,16 @@
+package slim.bootjava.rest;
+
+import java.time.LocalDateTime;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    
+    @GetMapping("/")
+    public String hello() {
+        return "Hello Spring Boot with Java - " + LocalDateTime.now();
+    }
+
+}
