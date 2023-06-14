cb5b6265240759121f98987b07142b313a0fde4d
Hoon <yanghoon5@gmail.com>
2023-06-14 23:25:00 +0900

    Add REST Client Config with Proxy

--- src/main/java/slim/bootjava/config/RetrofitConfig.java
+++ src/main/java/slim/bootjava/config/RetrofitConfig.java
@@ -1,10 +1,14 @@
 package slim.bootjava.config;
 
+import java.net.InetSocketAddress;
+import java.net.Proxy;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import com.jakewharton.retrofit2.adapter.reactor.ReactorCallAdapterFactory;
 
+import okhttp3.OkHttpClient;
 import retrofit2.Retrofit;
 import retrofit2.converter.jackson.JacksonConverterFactory;
 import slim.bootjava.domain.post.repository.PostRetrofitRepository;
@@ -17,15 +21,34 @@ public class RetrofitConfig {
 
     private String baseUrl = "https://jsonplaceholder.typicode.com";
     
+    private boolean proxyEnable = false;
+    private String proxyHost = "127.0.0.1";
+    private int proxyPost = 8080;
+    
     @Bean
     public PostRetrofitRepository postRetrofitRepository() {
-        Retrofit retrofit = new Retrofit.Builder()
+        return retrofit().create(PostRetrofitRepository.class);
+    }
+
+    @Bean
+    public Retrofit retrofit() {
+        Retrofit.Builder builder = new Retrofit.Builder()
             .baseUrl(baseUrl)
             .addConverterFactory(JacksonConverterFactory.create())
-            .addCallAdapterFactory(ReactorCallAdapterFactory.create())
-            .build();
+            .addCallAdapterFactory(ReactorCallAdapterFactory.create());
+        
+        if (proxyEnable) {
+            builder.client(okHttpClient());
+        }
 
-        return retrofit.create(PostRetrofitRepository.class);
+        return builder.build();
+    }
+
+    private OkHttpClient okHttpClient() {
+        Proxy proxy = new Proxy(Proxy.Type.HTTP, new InetSocketAddress(proxyHost, proxyPost));
+        return new OkHttpClient.Builder()
+            .proxy(proxy)
+            .build();
     }
 
 }
--- src/main/java/slim/bootjava/config/WebClientConfig.java
+++ src/main/java/slim/bootjava/config/WebClientConfig.java
@@ -1,17 +1,40 @@
 package slim.bootjava.config;
 
+import java.net.InetSocketAddress;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.reactive.ReactorClientHttpConnector;
 import org.springframework.web.reactive.function.client.WebClient;
+
+import reactor.netty.http.client.HttpClient;
+import reactor.netty.transport.ProxyProvider;
 
 @Configuration
 public class WebClientConfig {
 
     private String baseUrl = "https://jsonplaceholder.typicode.com";
 
+    private boolean proxyEnable = false;
+    private String proxyHost = "127.0.0.1";
+    private int proxyPost = 8080;
+
     @Bean
-    public WebClient postWebClient() {
-        return WebClient.create(baseUrl);
+    public WebClient postWebClient(WebClient.Builder builder) {
+        builder = builder.clone();
+
+        builder.baseUrl(baseUrl);
+
+        if (proxyEnable) {
+            builder.clientConnector(new ReactorClientHttpConnector(connector()));
+        }
+
+        return builder.build();
     }
 
+    public HttpClient connector() {
+        return HttpClient.create().proxy(options -> {
+            options.type(ProxyProvider.Proxy.HTTP).address(new InetSocketAddress(proxyHost, proxyPost));
+        });
+    }
 }
